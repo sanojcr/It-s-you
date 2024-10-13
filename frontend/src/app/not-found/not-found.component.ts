6e7408c6c06987300af3e5c8fc856cd144f3afd0
sanojcr <sanoj.cr@gmail.com>
2024-10-13 23:08:47 +0530

    added not found page and some services

---
+++ frontend/src/app/not-found/not-found.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { ThemeService } from '../services/theme.service';
+import { SessionService } from '../services/session.service';
+
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrl: './not-found.component.scss'
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor(private route: Router,
+    private theme: ThemeService,
+    private session: SessionService
+  ) {
+    this.setDarkMode(theme.isDarkMode);
+  }
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+
+  ngAfterViewInit(): void {
+    initFlowbite();
+  }
+
+  setDarkMode(isDark: boolean) {
+    if (isDark) {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }
+
+  redirectToHome(){
+    this.theme.isDarkMode = false;
+    this.setDarkMode(this.theme.isDarkMode);
+    this.route.navigate(['/']);
+  }
+
+}
